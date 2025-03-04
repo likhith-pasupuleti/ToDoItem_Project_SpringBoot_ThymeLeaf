94d7dfa382b2f7dc13f32841762a634234d1e7d3
likhith-pasupuleti <likhithsaikumarpasupuleti@gmail.com>
2025-03-04 14:06:57 -0800

    intial commit

---
+++ src/test/java/com/springBootProject/todoListApp/TodoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootProject.todoListApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
